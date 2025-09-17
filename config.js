1d952b5fd801c996b72ea511938980c645ae3670
leadmoth <demetriszosi@gmail.com>
2025-09-17 20:20:02 +0100

    Initial commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Barebones config: the frontend will POST to this URL.
 // Default is the Vercel serverless proxy; set N8N_WEBHOOK_URL in Vercel.
 window.REPCRAFTER_CONFIG = {
-  WEBHOOK_URL: "/api/chat"
+  WEBHOOK_URL: "https://leadmoth.app.n8n.cloud/webhook/c61d0fe0-7162-4a5b-95ac-fc8b789106af/chat"
 };
